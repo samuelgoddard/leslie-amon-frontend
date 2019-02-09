98148da0a32e6570593a837318ae28edb7bbed1c
Sam Goddard <hello@samgoddard.co.uk>
2019-02-09 17:45:24 +0000

    split global layouts into default layout vue

---
+++ components/SiteFooter.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer class="m-footer sm:mb-8">
+    <div class="container">
+      <div class="flex flex-wrap items-center">
+        <div class="w-full md:w-2/5 bg-grey-lightest py-4 sm:p-6 container--bleed mb-6 md:mb-0">
+          <h3 class="text-xl font-light mb-4">Stay updated</h3>
+
+          <form class="w-full mb-5">
+            <div class="flex items-end">
+              <input class="appearance-none rounded-none bg-white border-b border-black w-full text-black py-2 px-4 focus:outline-none text-sm focus:border-transparent transition" type="email" placeholder="Enter Email..." aria-label="Email Address" required>
+              <input class="appearance-none rounded-none bg-white text-sm text-black hover:bg-black hover:text-white focus:bg-black focus:text-white py-2 px-4 border-b border-black" type="submit" value="Send" />
+            </div>
+          </form>
+
+          <a href="#" class="text-sm text-black">Instagram</a>
+          <a href="#" class="text-sm text-black ml-2">Facebook</a>
+        </div>
+
+        <div class="w-full md:w-3/5">
+          <div class="flex flex-wrap md:pl-10">
+            <div class="mb-8 sm:mb-0 w-1/2 sm:w-1/3">
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Contact</a></div>
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Stockists</a></div>
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Press</a></div>
+          </div>
+
+            <div class="mb-8 sm:mb-0 w-1/2 sm:w-1/3">
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Taxes &amp; Duties</a></div>
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Shipping Information</a></div>
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Returns</a></div>
+          </div>
+
+            <div class="mb-8 sm:mb-0 w-1/2 sm:w-1/3">
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Terms &amp; Conditions</a></div>
+              <div><a href="#" class="text-black text-sm no-underline hover:underline">Privacy</a></div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>  
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="leading-normal font-sans antialiased">
+    <site-header />
     <nuxt />
+    <site-footer />
   </div>
 </template>
+
+<script>
+  import SiteHeader from '~/components/SiteHeader.vue'
+  import SiteFooter from '~/components/SiteFooter.vue'
+
+  export default {
+    components: {
+      SiteHeader,
+      SiteFooter
+    },
+    data() {
+      return {
+      }
+    }
+  }
+</script>
