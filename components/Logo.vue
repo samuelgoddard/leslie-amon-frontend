cf28555a26781ae44693ce87208d42f683ebbc62
Sam Goddard <hello@samgoddard.co.uk>
2019-02-10 16:10:24 +0000

    weird safari bug with logo fixed

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <nuxt-link to="/" class="logo no-underline hover:opacity-75 transition block w-16 md:w-20">
-    <img src="~/assets/logo.png" alt="Leslie Amon Logo" class="w-full" />
+    <img src="/images/logo.jpg" alt="Leslie Amon Logo" class="w-full" />
   </nuxt-link>
 </template>
