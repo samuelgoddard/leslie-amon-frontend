d8c2dd12b454113ee4135dab0f0ecb5a3d2c25ad
Sam Goddard <hello@samgoddard.co.uk>
2019-02-10 16:06:19 +0000

    404 vue tidied up

--- pages/404.vue
+++ pages/404.vue
@@ -11,10 +11,20 @@
 
               <div class="w-full md:w-1/2 md:px-5 mb-5 md:mb-0">
                 <div class="md:px-6 lg:px-24">
-                  <h2 class="font-thin text-3xl md:text-4xl mb-3 md:mb-5">Page not found</h2> 
-                  <p class="text-lg lg:text-xl text-black mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis earum possimus nostrum deserunt, delectus distinctio.</p>
+                  <div class="mb-5">
+                    <h2 class="font-thin text-3xl xl:text-4xl mb-3 md:mb-5">Page not found</h2> 
+                    <p class="text-base lg:text-lg xl:text-xl text-black mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis earum possimus nostrum deserunt, delectus distinctio.</p>
 
-                  <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas natus illo harum non soluta dolor possimus rem quas voluptatum! Possimus.</p>
+                    <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas natus illo harum non soluta dolor possimus rem quas voluptatum! Possimus.</p>
+                  </div>
+
+                  <h3 class="font-thin text-xl mb-3">Navigate</h3>
+                  <nav>
+                    <nuxt-link to="/products" class="inline-block mr-3 text-black no-underline hover:underline text-sm" >Swimwear</nuxt-link>
+                    <nuxt-link to="/lookbook" class="inline-block mr-3 text-black no-underline hover:underline text-sm">Lookbook</nuxt-link>
+                    <nuxt-link to="/404" class="inline-block mr-3 text-black no-underline hover:underline text-sm">Press</nuxt-link>
+                    <nuxt-link to="/about" class="inline-block mr-3 text-black no-underline hover:underline text-sm">About</nuxt-link>
+                  </nav>
                 </div>
               </div>
             </div>
